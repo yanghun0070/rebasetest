e18924c31e810006ad06f29408eb2912a486589a
jeehun.yang <jeehun.yang@ahnlab.com>
2020-04-22 21:01:07 +0900

    commit test rebase 1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	    // write your code here
-        System.out.println("test3");
+        // write your code here
+        System.out.println("test Rebase 1");
     }
 }
