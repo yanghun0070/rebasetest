e033eb93151cc18cc0ed110a7a773541ce29916f
jeehun.yang <jeehun.yang@ahnlab.com>
2020-04-22 20:35:16 +0900

    add test code from feature rebase

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	    // write your code here
-        System.out.println("test");
+        System.out.println("test 를 위해 rebase 작업 진행");
     }
 }
