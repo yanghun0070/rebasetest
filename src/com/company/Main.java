54d2b0332f3c53fcc8a2fd66463ae29d63fb9d6c
jeehun.yang <jeehun.yang@ahnlab.com>
2020-04-26 18:42:24 +0900

    commit test !!! message

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         // write your code here
-        System.out.println("test 555");
+        System.out.println("test !!!");
     }
 }
